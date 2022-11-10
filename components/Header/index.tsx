5aa5f6df0c0536c8255fb943934f35dd0b8bb478
Phil <pbrede21@gmail.com>
2022-11-10 11:03:04 -0700

    ripped

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
-import Link from "next/link";
 import logo from "public/logo.png";
 
 export function Header({ className }) {
   return (
     <div className={className}>
-      <Link href="/">
+      <a
+        href="https://www.twilio.com"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
         <Image alt="logo" src={logo} height={47} width={47 * (1005 / 305)} />
-      </Link>
+      </a>
+
       <div />
     </div>
   );
